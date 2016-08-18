b3853acb9a137b95b5b4653ff6ca066fb0f35baf
Wouerner <12307444793@cultura.gov.br>
2016-08-18 16:34:24 -0300

    arquivos que estavam faltando

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,8 +26,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-
-        //return view('home');
         return view('home', ['projetos' => Projeto::where('users_id', Auth::user()->id)->get()]);
     }
 }
